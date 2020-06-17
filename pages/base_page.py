ee74dec795369d6fc91cee2a74961b4611020ee6
Mira <miraukra@gmail.com>
2020-06-17 18:38:41 +0300

    add solve_quiz_and_get_code

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,12 @@
 from selenium.webdriver import Remote as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
 
 
 class BasePage():
@@ -17,3 +24,17 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split (" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("NO second alert presented")
