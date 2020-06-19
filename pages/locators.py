87e9bb4e81e18799383f7fed4dd7c3eb532fcfd9
Mira <miraukra@gmail.com>
2020-06-19 19:12:01 +0300

    delete commented locators

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,7 @@ class BasePageLocators:
     BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a.btn.btn-default")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
+
 class MainPageLocators:
     pass
 
@@ -13,9 +14,6 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-    # EMAIL_ADDRESS_LOGIN_TEXTFIELD = (By.CSS_SELECTOR, "id_login-username")
-    # PASSWORD_LOGIN_TEXTFIELD = (By.CSS_SELECTOR, "#id_login-password")
-    # LOGIN_BUTTON = (By.CSS_SELECTOR, "#login_form .btn-lg")
     EMAIL_ADDRESS_REGISTER_TEXTFIELD = (By.CSS_SELECTOR, "#id_registration-email")
     PASSWORD_REGISTER_TEXTFIELD = (By.CSS_SELECTOR, "#id_registration-password1")
     CONFIRM_PASSWORD_REGISTER_TEXTFIELD = (By.CSS_SELECTOR, "#id_registration-password2")
