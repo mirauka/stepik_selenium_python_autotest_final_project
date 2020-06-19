4f84d039f7032da4ce93890b2c450571a949c760
Mira <miraukra@gmail.com>
2020-06-19 17:34:29 +0300

    add register_new_user method

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,26 +3,26 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_textfield = self.browser.find_element(*LoginPageLocators.EMAIL_ADDRESS_REGISTER_TEXTFIELD)
+        email_textfield.send_keys(email)
+        password_textfield = self.browser.find_element(*LoginPageLocators.PASSWORD_REGISTER_TEXTFIELD)
+        password_textfield.send_keys(password)
+        confirm_password_textfield = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD_REGISTER_TEXTFIELD)
+        confirm_password_textfield.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert "/login/" in self.browser.current_url, "Login url is incorrect"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        # self.is_element_present(*LoginPageLocators.EMAIL_ADDRESS_LOGIN_TEXTFIELD)
-        # self.is_element_present(*LoginPageLocators.PASSWORD_LOGIN_TEXTFIELD)
-        # self.is_element_present(*LoginPageLocators.LOGIN_BUTTON)
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        # self.is_element_present(*LoginPageLocators, EMAIL_ADDRESS_REGISTER_TEXTFIELD)
-        # self.is_element_present(*LoginPageLocators, PASSWORD_REGISTER_TEXTFIELD)
-        # self.is_element_present(*LoginPageLocators, CONFIRM_PASSWORD_REGISTER_TEXTFIELD)
-        # self.is_element_present(*LoginPageLocators, REGISTER_BUTTON)
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present"
