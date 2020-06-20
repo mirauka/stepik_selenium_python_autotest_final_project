44b57aee2387f90ebf69d2a649517a394aaaf603
Mira <miraukra@gmail.com>
2020-06-20 15:56:55 +0300

    adding 2 arguments(book_name, book_price) to should_be_success_message method

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,9 +7,9 @@ class ProductPage(BasePage):
         button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         button.click()
 
-    def should_be_success_message(self):
-        self.should_be_correct_book()
-        self.should_be_correct_price()
+    def should_be_success_message(self, book_name, book_price):
+        self.should_be_correct_book(book_name)
+        self.should_be_correct_price(book_price)
 
     def return_book_name(self):
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
@@ -19,17 +19,17 @@ class ProductPage(BasePage):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         return book_price
 
-    def should_be_correct_book(self):
+    def should_be_correct_book(self, book_name):
         success_message_book_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_BOOK_NAME).text
-        assert self.return_book_name() == success_message_book_name, f"Incorrect book name in success message. Should be: '{self.return_book_name()}', you got: '{success_message_book_name}'"
+        assert book_name == success_message_book_name, f"Incorrect book name in success message. Should be: '{book_name}', you got: '{success_message_book_name}'"
 
-    def should_be_correct_price(self):
+    def should_be_correct_price(self, book_price):
         success_message_book_price = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_BOOK_PRICE).text
-        assert self.return_book_price() == success_message_book_price, f"Incorrect book price in success message. Should be: '{self.return_book_price()}', you got: '{success_message_book_price}'"
+        assert book_price == success_message_book_price, f"Incorrect book price in success message. Should be: '{book_price}', you got: '{success_message_book_price}'"
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented"
 
     def should_be_disappeared_success_message(self):
-        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, should be disappeared"
-
+        assert self.is_disappeared(
+            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, should be disappeared"
