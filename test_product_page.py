f88e4d82c9e04ce4f0fa59c3089d806b6fa4abb5
Mira <miraukra@gmail.com>
2020-06-20 16:03:05 +0300

    formatting

--- test_product_page.py
+++ test_product_page.py
@@ -23,7 +23,7 @@ class TestUserAddToBasketFromProductPage:
         product_page = ProductPage(browser, link)
         product_page.open()
         product_page.should_not_be_success_message()
-#
+
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
