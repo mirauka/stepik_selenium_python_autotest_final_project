1d960a2eafe12429057ff3847dd85f06899d3476
Mira <miraukra@gmail.com>
2020-06-20 13:21:36 +0300

    add final checking to test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -91,6 +91,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 @pytest.mark.need_review
